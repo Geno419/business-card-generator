6f5734e6a8597eb7c8c8ef412404538fb0ab92d3
lauriehillier <laurie.hillier@gmail.com>
2024-01-23 15:54:52 +0000

    add business card page and styling

--- submit.js
+++ submit.js
@@ -1,4 +1,18 @@
 const string = document.location.search;
 const queries = new URLSearchParams(string);
 
-console.log(queries.get("first-name"));
+const firstName = queries.get("first-name");
+const lastName = queries.get("last-name");
+const companyName = queries.get("company");
+const jobTitle = queries.get("job-title");
+const email = queries.get("email");
+const phoneNumber = queries.get("phone-number");
+
+const inputName = document.getElementById("name");
+const title = document.getElementById("title");
+inputName.innerText = `${firstName} ${lastName}`;
+title.innerText = `${jobTitle}`;
+const contactInfo = document.getElementById("contact-info");
+contactInfo.innerHTML = `Email: ${email}<br>Phone: ${phoneNumber}`;
+const logo = document.getElementById("logo");
+logo.innerHTML = `${companyName}`;
